971367fc3426cbe033caf025b89ae5809e2ff426
asuther <alexander.m.sutherland@gmail.com>
2015-01-11 20:35:00 -0800

    Minor change

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+
+module.exports = function(grunt) {
+
+
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: 'js/**/*.js',
+                dest: 'build/fullJS.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+};
